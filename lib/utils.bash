0446ed15cb659145d630b87f3eaac37990373e99
Pierre Demagny <pierre.demagny@blobr.io>
2023-01-23 11:21:34 +0100

    feat: initial release

--- lib/utils.bash
+++ lib/utils.bash
@@ -2,7 +2,6 @@
 
 set -euo pipefail
 
-# TODO: Ensure this is the correct GitHub homepage where releases can be downloaded for serverless.
 GH_REPO="https://github.com/serverless/serverless"
 TOOL_NAME="serverless"
 TOOL_TEST="serverless --version"
@@ -10,6 +9,16 @@ TOOL_TEST="serverless --version"
 fail() {
   echo -e "asdf-$TOOL_NAME: $*"
   exit 1
+}
+
+get_os() {
+  os=$(uname -s)
+  case $os in
+  Darwin) os="darwin" ;;
+  Linux) os="linux" ;;
+  *) fail "The os (${os}) is not supported by this installation script." ;;
+  esac
+  echo "$os"
 }
 
 curl_opts=(-fsSL)
@@ -31,7 +40,6 @@ list_github_tags() {
 }
 
 list_all_versions() {
-  # TODO: Adapt this. By default we simply list the tag names from GitHub releases.
   # Change this function if serverless has other means of determining installable versions.
   list_github_tags
 }
@@ -41,8 +49,8 @@ download_release() {
   version="$1"
   filename="$2"
 
-  # TODO: Adapt the release URL convention for serverless
-  url="$GH_REPO/archive/v${version}.tar.gz"
+  os=$(get_os)
+  url="$GH_REPO/releases/download/v${version}/serverless-${os}-x64"
 
   echo "* Downloading $TOOL_NAME release $version..."
   curl "${curl_opts[@]}" -o "$filename" -C - "$url" || fail "Could not download $url"
@@ -58,12 +66,12 @@ install_version() {
   fi
 
   (
-    mkdir -p "$install_path"
-    cp -r "$ASDF_DOWNLOAD_PATH"/* "$install_path"
-
-    # TODO: Assert serverless executable exists.
     local tool_cmd
     tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
+
+    mkdir -p "$install_path"
+    cp -r "$ASDF_DOWNLOAD_PATH/${tool_cmd}-${version}" "$install_path/$tool_cmd"
+
     test -x "$install_path/$tool_cmd" || fail "Expected $install_path/$tool_cmd to be executable."
 
     echo "$TOOL_NAME $version installation was successful!"
